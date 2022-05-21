eeb96b00da275a69933d2027205795c74149b490
Vadim <vadim_kimm@icloud.com>
2022-05-21 22:19:56 +0300

    Added method changeCircularPathColor()

--- HW-12/CircularProgressBarView.swift
+++ HW-12/CircularProgressBarView.swift
@@ -60,5 +60,10 @@ class CircularProgressBarView: UIView {
         }
 
 
+    func changeCircularPathColor(to color: CGColor) {
+        progressLayer.strokeColor = color
+        circleLayer.strokeColor = color
+    }
+
 }
 
